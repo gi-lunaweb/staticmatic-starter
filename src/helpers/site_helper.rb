9ca0f8bb3875a499e5a218b2d9539788732e705f
Gaël-Ian Havard <gael-ian@lunaweb.fr>
2011-03-03 11:10:06 +0100

    Ajout de helpers
    Modification du fonctionnement interne de content_for pour utiliser un Hash plutôt que plusieurs variables d'instance.
    Lorsque content_for est appelé sans lui fournir de contenu, il retourne le contenu précédemment défini pour la clé demandée.
    
    Ajout de helpers seo_title(), seo_description() et body_class(), qui fonctionnent tous de la même façon que content_for.
    body_class() sépare automatiquement les classes par des espaces, seo_title() sépare automatiquement les éléments du titre par des tirets, l'un comme l'autre supprime les caractères inutiles en début et fin de chaine lorsque leur contenu est demandé.
    
    Modification des fichiers par défaut pour utiliser les nouveaux helpers

--- src/helpers/site_helper.rb
+++ src/helpers/site_helper.rb
@@ -1,5 +1,25 @@
 module SiteHelper
-  def content_for(name, &block) 
-    instance_variable_set("@content_for_#{name}", capture_haml(&block)) 
+
+  def content_for(name, content = nil, &block)
+    @_content_for = {} unless instance_variable_defined? :@_content_for
+    @_content_for[name] = String.new unless @_content_for.key? name
+
+    content = capture_haml(&block) if block_given?
+    @_content_for[name] << content if content
+    @_content_for[name] unless content
+  end
+
+  def seo_title(title = nil)
+    content_for(:seo_title, "#{title} - ") if title
+    content_for(:seo_title).gsub(/^([\s-]+)/, '').gsub(/([\s-]+)$/, '') unless title
+  end
+
+  def seo_description(desc = nil)
+    content_for(:seo_description, desc)
+  end
+
+  def body_class(klass = nil)
+    content_for(:body_class, " #{klass}") if klass
+    content_for(:body_class).strip unless klass
   end
 end
