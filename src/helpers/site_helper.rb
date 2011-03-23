cf84afe1e54087467a39609e52625ab9fb304e82
Aimerick Desdoit <aimerick@lunaweb.fr>
2011-03-23 17:25:20 +0100

    ajout des helpers navigation sitemap et link_to

--- src/helpers/site_helper.rb
+++ src/helpers/site_helper.rb
@@ -22,4 +22,100 @@ module SiteHelper
     content_for(:body_class, " #{klass}") if klass
     content_for(:body_class).strip unless klass
   end
+
+  def navigation(level = 1)
+    ul_li pages_yml, level * 2
+  end
+
+  def sitemap(level = 3)
+    active = @current_page || nil
+    
+    nav = "<dl>\n"
+    pages_yml.each do |page|
+      nav << %(  <dt>#{page['title']}</dt>\n)
+      nav << %(  <dd>\n)
+      if page['children']
+        nav_children, active = ul_li(page['children'], level * 2, 2)
+        nav_children.gsub!(' class="active"', '')
+        
+        nav << nav_children
+      end
+      nav << %(  </dd>\n)
+    end
+    nav << '</dl>'
+  end
+
+  def link_to(name, label = nil)
+    get = lambda do |pages|
+      find = nil
+      pages.each do |page|
+        if page['name'] == name.to_s
+          find = page
+          break
+        else
+          child = get.call page['children'] if page['children']
+          if child
+            find = child
+            break
+          end
+        end
+      end
+      find
+    end
+    
+    page = get.call pages_yml
+    if page
+      label ||= page['title']
+      return %(<a href="#{page['url']}">#{label}</a>)
+    end
+    ''
+  end
+
+  private
+  
+  def ul_li(pages, max_indice = 2, indice = 0)
+    active = @current_page || nil
+    
+    has_active = false
+    prefix = "  " * indice
+    nav = %(#{prefix}<ul>\n)
+    
+    pages.each_with_index do |page, i|
+      has_active = (active == page['name'])
+      if page['children']
+        nav_children = ''
+        if max_indice > (indice + 2)
+          nav_children, active = ul_li(page['children'], max_indice, indice + 2)
+          has_active ||= active
+        end
+      end
+      
+      id = indice == 0 ? %( id="n#{i + 1}") : ''
+      klass = has_active ? %( class="active") : ''
+      
+      nav << %(#{prefix}  <li#{id}#{klass}>\n)
+      nav << %(#{prefix}    <a href="#{page['url']}">#{page['title']}</a>)
+      
+      if page['children'] && !nav_children.blank?
+        nav << "\n#{nav_children}"
+      else
+        nav << "\n"
+      end
+      
+      nav << %(#{prefix}  </li>\n)
+    end
+    
+    nav << %(#{prefix}</ul>\n)
+    
+    if indice == 0
+      nav
+    else
+      [nav, has_active]
+    end
+  end
+  
+  def pages_yml
+    @pages ||= YAML.load(File.open(File.expand_path('../../../config/pages.yml', __FILE__)))
+  end
+  
 end
